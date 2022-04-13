b7ab512b696cd2718ed77e0e84ee506a36a3d774
Nicols <nicole.gamboacifuentes@gmail.com>
2022-04-12 21:10:25 -0400

    migracion base de datos

---
+++ database/data.sql
@@ -0,0 +1,43 @@
+--1 Migracion tabla Usuario 
+
+CREATE TABLE IF NOT EXISTS usuarios (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(200),
+    apellido VARCHAR(200),
+    password VARCHAR(200),
+    email VARCHAR(200),
+    tipo_usuario VARCHAR(200)
+);
+
+
+--2. Migracion tabla productos
+
+CREATE TABLE  IF NOT EXISTS productos (
+    id SERIAL PRIMARY KEY,
+    producto VARCHAR(200),
+    descripcion VARCHAR(200),
+    stock INT,
+    valor INT
+);
+
+--3. Migracion tabla Compras
+
+CREATE TABLE IF NOT EXISTS compras (
+    id SERIAL PRIMARY KEY,
+    usuario_id_fk INT ,
+    fecha DATE,
+    total INT,
+    foreign key(usuario_id_fk) references usuarios (id)
+);
+
+--3. Migracion tabla detalle compras
+
+CREATE TABLE IF NOT EXISTS detalle_compras (
+    id SERIAL PRIMARY KEY,
+    compra_id_fk INT ,
+    producto_id_fk INT ,
+    CANTIDAD INT,
+    PRECIO INT,
+    FOREIGN KEY(compra_id_fk) references compras(id), 
+    FOREIGN KEY(producto_id_fk) references productos (id)
+);
---
+++ database/migrate.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const { Pool } = require("pg");
+const fs = require("fs");
+
+const pool = new Pool(
+    process.env.DATABASE_URL && {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+    }
+);
+
+const migrate = async () => {
+    try {
+        const data = fs.readFileSync(__dirname + "/data.sql", {
+            encoding: "utf-8",
+        });
+        await pool.query(data);
+        console.log("tabla creada");
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+migrate().then(console.log)
+
+
+module.exports = pool
